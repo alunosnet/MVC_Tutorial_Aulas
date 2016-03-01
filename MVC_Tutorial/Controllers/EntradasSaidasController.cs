559bc2d3cd08730012ef95cb1ce097106d6e8904
alunosnet <alunosnet@gmail.com>
2016-03-01 08:46:33 +0000

    Versão final atualizada

---
+++ MVC_Tutorial/Controllers/EntradasSaidasController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Hotel.Models;
+
+namespace MVC_Tutorial.Controllers
+{
+    [Authorize]
+    public class EntradasSaidasController : Controller
+    {
+        EntradaSaidaBD bd = new EntradaSaidaBD();
+
+        // GET: EntradasSaidas
+        public ActionResult Index()
+        {
+            //lista dos quartos ocupados
+            return View(bd.listaOcupados());
+        }
+
+        public ActionResult Entrada()
+        {
+            //lista clientes
+            ClientesBD bdClientes = new ClientesBD();
+            ViewBag.listaClientes = bdClientes.lista();
+            //lista quartos
+            QuartosBD bdQuartos = new QuartosBD();
+            ViewBag.listaQuartos = bdQuartos.listaVazios();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Entrada(EntradaSaidaModel novo)
+        {
+            if (ModelState.IsValid)
+            {
+                bd.registarEntrada(novo);
+                return RedirectToAction("Index");
+            }
+            return View(novo);
+        }
+
+        public ActionResult Saida(int id)
+        {
+            return View(bd.listaOcupados(id)[0]);
+        }
+        [HttpPost]
+        public ActionResult Saida(EntradaSaidaModel saida)
+        {
+            bd.registarSaida(saida);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC_Tutorial/Controllers/QuartosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Hotel.Models;
+
+namespace MVC_Tutorial.Controllers
+{
+    [Authorize]
+    public class QuartosController : Controller
+    {
+        QuartosBD bd = new QuartosBD();
+
+        // GET: Quartos
+        public ActionResult Index()
+        {
+            if (Session["perfil"].ToString() != "0") return RedirectToAction("Index", "Home");
+
+            return View(bd.lista());
+        }
+
+        public ActionResult Create()
+        {
+            if (Session["perfil"].ToString() != "0") return RedirectToAction("Index", "Home");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(QuartosModel dados)
+        {
+            if (ModelState.IsValid)
+            {
+                bd.adicionarQuarto(dados);
+                return RedirectToAction("Index");
+            }
+            return View(dados);
+        }
+    }
+}
